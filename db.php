21db17db018b7732cdee88645742e637776eb0e2
Priyam7781 <139098654+Priyam7781@users.noreply.github.com>
2023-07-10 22:08:36 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+ $databaseHost = 'localhost';
+ $databaseName = 'ohms';
+ $databaseUsername = 'root';
+ $databasePassword = '';
+
+   $conn = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+   ?>
+
+<!-- 
+$user = 'root';
+$password = '';
+$db = 'ohms';
+$host = 'localhost';
+$port = 8888;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+); -->
+
